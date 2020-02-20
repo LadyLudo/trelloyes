a5b12e12b4c6a2cf26493730e4bcea4dcc7f3ad3
Catherine Davis <catherinevtdavis@gmail.com>
2020-02-20 12:20:09 -0700

    Added tests for two components

--- src/App.js
+++ src/App.js
@@ -13,20 +13,20 @@ class App extends React.Component {
 
     render() {
         const {store} = this.props
-      return (
-          <main className="App">
-              <header className="App-header">
-                  <h1>Trelloyes!</h1>
-              </header>
-              <div className="App-list">
-                  {store.lists.map(list =>
-                      <List
-                          key={list.id}
-                          header={list.header}
-                          cards={list.cardIds.map(id=> store.allCards[id])} />
-                  )}
-              </div>
-          </main>
+          return (
+              <main className="App">
+                  <header className="App-header">
+                      <h1>Trelloyes!</h1>
+                  </header>
+                  <div className="App-list">
+                      {this.props.store.lists.map(list =>
+                          <List
+                              key={list.id}
+                              header={list.header}
+                              cards={list.cardIds.map(id=> store.allCards[id])} />
+                      )}
+                  </div>
+              </main>
       );
     }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+import List from './List';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+//  Smoke test - card
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Card />, div);
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+//  Smoke test - list
+// it('renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<List />, div);
+//   ReactDOM.unmountComponentAtNode(div)
+// })
+
+
+// Snapshot test - card
+it('renders UI as expected', () => {
+  const tree = renderer
+      .create(<Card />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+// Snapshot test - list
+// it('renders UI as expected', () => {
+//   const tree = renderer
+//       .create(<List />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
